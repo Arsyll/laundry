fc7ff12ed69eb881eeec4284554d51e92c4b7027
Arsyll <arsylslametamrulloh@gmail.com>
2023-03-27 07:16:28 +0700

    last

--- app/Charts/BulananChart.php
+++ app/Charts/BulananChart.php
@@ -17,28 +17,28 @@ class BulananChart
     public function build()
     {
         $transaksi = DB::table('transaksi')
-->select(DB::raw('MONTH(created_at) AS bulan'), DB::raw('COUNT(id) AS jml'))
-->whereYear('created_at', '=', date('Y'))
-->groupBy('bulan')
-->get();
+            ->select(DB::raw('MONTH(created_at) AS bulan'), DB::raw('COUNT(id) AS jml'))
+            ->whereYear('created_at', '=', date('Y'))
+            ->groupBy('bulan')
+            ->get();
         $data['bulan'] = [];
-    $data['nilaiB'] = [];
+        $data['nilaiB'] = [];
 
 
-for ($i = 1; $i <= 12; $i++) {
-    $data['bulan'][] = date('F', mktime(0, 0, 0, $i, 1)); // ambil nama bulan dengan format lengkap
-    $found = false;
-    foreach ($transaksi as $t) {
-        if ((int)$t->bulan === $i) {
-            $data['nilaiB'][] = (int)$t->jml;
-            $found = true;
-            break;
+    for ($i = 1; $i <= 12; $i++) {
+        $data['bulan'][] = date('F', mktime(0, 0, 0, $i, 1)); // ambil nama bulan dengan format lengkap
+        $found = false;
+        foreach ($transaksi as $t) {
+            if ((int)$t->bulan === $i) {
+                $data['nilaiB'][] = (int)$t->jml;
+                $found = true;
+                break;
+            }
+        }
+        if (!$found) {
+            $data['nilaiB'][] = 0;
         }
     }
-    if (!$found) {
-        $data['nilaiB'][] = 0;
-    }
-}
 
     return $this->chart->lineChart()
         ->addData('Laundry Masuk', $data['nilaiB'])
